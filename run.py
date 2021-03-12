61f0ccf19df1905e431afc49967cd437c43e9a62
U188 <54225629+U188@users.noreply.github.com>
2021-03-12 11:07:54 +0800

    Update run.py

--- run.py
+++ run.py
@@ -74,7 +74,7 @@ def telegram_bot (OO0O0OOOOO00O0O00 ,O00000O0O000OOOO0 ):#line:97
     OO0OO00OOOOO0O0OO =TG_BOT_TOKEN #line:99
     O0000OOO00OOO000O =TG_USER_ID #line:100
     if "TG_USER_ID"in os .environ :#line:101
-        OO0OO00OOOOO0O0OO ='1698539466:AAG4K86swWWty6AxeHf58sifhfjXhusiqCM'#line:102
+        OO0OO00OOOOO0O0OO =os .environ ["TG_BOT_TOKEN"]
         O0000OOO00OOO000O =os .environ ["TG_USER_ID"]#line:103
     if not OO0OO00OOOOO0O0OO or not O0000OOO00OOO000O :#line:104
         print ("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")#line:105
